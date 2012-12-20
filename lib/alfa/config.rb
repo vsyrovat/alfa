484d108b6a97637fc9acec5e7d6906d6307c13d8
Valentin Syrovatskiy <vsyrovat@gmail.com>
2012-12-20 21:00:03 +1100

    + Tests for Alfa::Config

--- lib/alfa/config.rb
+++ lib/alfa/config.rb
@@ -6,5 +6,19 @@ module Alfa
       self[:log] = {}
     end
 
+    def []=(key, value)
+      if [:db, :log].include? key
+        raise "key :#{key} should include Enumerable" unless value.class.included_modules.include? Enumerable
+      end
+      super
+    end
+
+    def store(key, value)
+      if [:db, :log].include? key
+        raise "key :#{key} should include Enumerable" unless value.class.included_modules.include? Enumerable
+      end
+      super
+    end
+
   end
 end
---
+++ test/test_config.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'alfa/config'
+
+class AlfaConfigTest < Test::Unit::TestCase
+  def test_01
+    config = Alfa::Config.new
+    assert config.is_a?(Hash)
+    assert config[:db].is_a?(Hash)
+    assert config[:log].is_a?(Hash)
+    assert_raise RuntimeError do
+      config[:db] = nil
+    end
+    assert_raise RuntimeError do
+      config.store(:db, nil)
+    end
+    assert_raise RuntimeError do
+      config[:log] = nil
+    end
+    assert_raise RuntimeError do
+      config.store(:log, nil)
+    end
+    assert_equal({:db=>{}, :log=>{}}, config)
+    config[:foo] = 1
+    assert_equal({:db=>{}, :log=>{}, :foo=>1}, config)
+  end
+end
