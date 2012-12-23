e51c6598b3865e4893a48c3efbf7eeca8db68237
Valentin Syrovatskiy <vsyrovat@gmail.com>
2012-12-24 01:56:03 +1100

    + Tests for Alfa::Application class

---
+++ test/test_application.rb
@@ -0,0 +1,22 @@
+require 'test/unit'
+require 'alfa/application'
+require 'alfa/config'
+
+class AlfaApplicationTest < Test::Unit::TestCase
+  def test_01 # base test
+    assert Alfa::VARS.is_a?(Hash), "Alfa::VARS expected to be a Hash"
+    assert_raise NoMethodError do
+      application = Alfa::Application.new
+    end
+    assert Alfa::Application.respond_to?(:init!)
+    assert Alfa::Application.respond_to?(:load_tasks)
+  end
+
+  def test_02 # Alfa::Application.config
+    assert Alfa::Application.config.is_a?(Alfa::Config)
+    Alfa::Application.config :foo => 1
+    assert_equal(1, Alfa::Application.config[:foo])
+    Alfa::Application.config[:bar] = 2
+    assert_equal(2, Alfa::Application.config[:bar])
+  end
+end
