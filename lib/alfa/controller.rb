7ac08f5a9e4d5ae8d906da2393f64755bd0c588b
Valentin Syrovatskiy <vsyrovat@gmail.com>
2013-01-04 18:33:30 +1100

    handle WebApplication instance by controllers

--- lib/alfa/controller.rb
+++ lib/alfa/controller.rb
@@ -1,5 +1,7 @@
 module Alfa
   class Controller
+    attr_accessor :application
+
     def _instance_variables_hash
       Hash[instance_variables.map { |name| [name.to_s[1..-1].to_sym, instance_variable_get(name)] } ]
     end
--- lib/alfa/web_application.rb
+++ lib/alfa/web_application.rb
@@ -70,6 +70,7 @@ module Alfa
             controller = self.invoke_controller(app_sym, c_sym)
             raise Exceptions::Route404 unless controller.class.instance_methods(false).include?(a_sym)
             controller._clear_instance_variables
+            controller.application = self
             controller.__send__(a_sym)
             data = controller._instance_variables_hash
             Ruty::Tags::RequireStyle.clean_cache
