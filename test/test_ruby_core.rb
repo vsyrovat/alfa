13d64588f8bc387fd90247abec54a34c7ad2e404
Valentin Syrovatskiy <vsyrovat@gmail.com>
2014-05-10 16:51:55 +1100

    ~ Switch to Ruby 2.1.2 as default
    ~ Switch to Ruby 2.1.2 as default

--- test/test_ruby_core.rb
+++ test/test_ruby_core.rb
@@ -19,4 +19,14 @@ EOL
     assert_equal(2, B.foo)
     assert_equal(3, C.foo)
   end
+
+  # Test for Regression of Hash#reject in Ruby 2.1.1
+  # https://www.ruby-lang.org/en/news/2014/03/10/regression-of-hash-reject-in-ruby-2-1-1/
+  # Should pass in Ruby 2.0.0 and 2.1.2+ (not in 2.1.1)
+  def test_02
+    eval <<EOL
+class SubHash < Hash; end
+EOL
+    assert_equal(SubHash, SubHash.new.reject{}.class)
+  end
 end
